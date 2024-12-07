7f2b71f1a4e6fff454efd6527252c227d41ec111
Waris Aremou <warisaremou2003@gmail.com>
2024-12-07 11:23:07 +0100

    update seeder to add admin user

--- database/seeders/seed.js
+++ database/seeders/seed.js
@@ -1,4 +1,5 @@
 import { db } from "../../config/database.js";
+import bcrypt from "bcrypt";
 
 async function seedDatabase() {
 	try {
@@ -8,6 +9,11 @@ async function seedDatabase() {
 		await db.execute(`INSERT INTO roles(name) VALUES ('user')`);
 		// -------- Create moderator role -------- //
 		await db.execute(`INSERT INTO roles(name) VALUES ('moderator')`);
+		// -------- Create an admin user -------- //
+		const hashedPassword = await bcrypt.hash("@AdminPassword1234", 10);
+		await db.execute(
+			`INSERT INTO users(username, surname, firstname, email, password, id_role) VALUES ('admin01','User','Admin','adminuser@example.com','${hashedPassword}', 1)`
+		);
 
 		console.log("✅ Database seeded successfully ✅");
 		process.exit(0);
