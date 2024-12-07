76bea2b3e987e73a262ca87768cf7ac430fccd99
Waris Aremou <warisaremou2003@gmail.com>
2024-12-07 21:41:48 +0100

    add hashHelper function

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+
 /**
  * Helper function to manage pagination
  */
@@ -8,5 +10,25 @@ export const pagination = async (page = 1) => {
 		page: (page - 1) * per_page,
 		currentPage: parseInt(page),
 		per_page,
+	};
+};
+
+/**
+ * Helper function to hash a value or compare a hashedValue
+ */
+export const hashHelper = () => {
+	const hash = async (value) => {
+		const hashedValue = await bcrypt.hash(value, 10);
+		return hashedValue;
+	};
+
+	const compare = async (value, hashedValue) => {
+		const isValid = await bcrypt.compare(value, hashedValue);
+		return isValid;
+	};
+
+	return {
+		hash,
+		compare,
 	};
 };
