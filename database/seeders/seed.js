e23f6fc22c9062ff101cba23cdea9dc68cd498e8
Yvanna <yvibeks099@gmail.com>
2024-12-09 14:59:12 +0100

    Create Posts

--- database/seeders/seed.js
+++ database/seeders/seed.js
@@ -25,7 +25,10 @@ async function seedDatabase() {
 		await db.execute(
 			`INSERT INTO users(username, surname, firstname, email, password, id_role) VALUES ('admin01','User','Admin','adminuser@example.com','${hashedPassword}', 1)`
 		);
-
+		
+		// ---------Create Posts---------//
+		await db.execute(`INSERT INTO posts( title, image, content, id_user) VALUES ('Bienvenus à vous','https://img.freepik.com/photos-premium/modele-conception-post-medias-sociaux-du-restaurant_1029473-146673.jpg?w=740','Plongez dans une expérience culinaire unique où saveurs et convivialité se rencontrent. Que vous soyez amateur de plats traditionnels ou curieux de découvrir des créations originales, notre chef met tout son savoir-faire dans chaque assiette.',1)`);
+		
 		console.log("✅ Database seeded successfully ✅");
 		process.exit(0);
 	} catch (error) {
