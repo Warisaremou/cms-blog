0a1eed4f3d05793bda4a02db4d3aa771a07eae8d
Waris Aremou <warisaremou2003@gmail.com>
2024-12-11 22:36:45 +0100

    reformat comment middleware

--- middleware/comment_middleware.js
+++ middleware/comment_middleware.js
@@ -6,7 +6,7 @@ import { commentExist } from "../services/comment_service.js";
  * Middleware to identify if user is moderator or comment owner before performing action
  */
 const isModeratorOrCommentOwner = async (req, res, next) => {
-	const {GET_ROLE_BY_ID} = authQueries()
+	const { GET_ROLE_BY_ID } = authQueries();
 	try {
 		const userData = await req.user;
 		const id_comment = await req.params.id;
@@ -14,24 +14,23 @@ const isModeratorOrCommentOwner = async (req, res, next) => {
 
 		if (!isCommentExist.exist) {
 			return res.status(404).json({
-				message : "Comment not found"
-			})
+				message: "Comment not found",
+			});
 		}
-	console.log(userData);
-	const [role] = await db.execute(GET_ROLE_BY_ID, [userData.id_role]);
-	const isCommentOwner = await isCommentExist.data.id_user === userData.id_user
-	
-	if (role[0].name !== "moderator" && !isCommentOwner) {
-			return res.status(403).json({
-				message : "Not Allowed"
-			})
-	}
 
-   next()
+		const [role] = await db.execute(GET_ROLE_BY_ID, [userData.id_role]);
+		const isCommentOwner = await isCommentExist.data.id_user == userData.id_user;
+
+		if (role[0].name !== "moderator" && !isCommentOwner) {
+			return res.status(403).json({
+				message: "Not Allowed",
+			});
+		}
+
+		next();
 	} catch (error) {
 		return res.status(401).json({ error });
 	}
 };
 
 export { isModeratorOrCommentOwner };
-
