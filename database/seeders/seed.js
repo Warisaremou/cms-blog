c908990cd07065dadfea45afcc52274ca296477e
Waris Aremou <warisaremou2003@gmail.com>
2024-12-11 22:34:55 +0100

    update post seeder

--- database/seeders/seed.js
+++ database/seeders/seed.js
@@ -19,7 +19,7 @@ async function seedDatabase() {
 			('Gastronomie et cuisine'),
 			('Futur et prospective')
 		`);
-		
+
 		// -------- Create an admin user -------- //
 		const hashedPassword = await bcrypt.hash("@AdminPassword1234", 10);
 		await db.execute(
@@ -27,15 +27,16 @@ async function seedDatabase() {
 		);
 
 		// -------- Create user -------- //
-		 const userHashedPassword = await bcrypt.hash("@User1234", 10);
-		 await db.execute(
+		const userHashedPassword = await bcrypt.hash("@User1234", 10);
+		await db.execute(
 			`INSERT INTO users(username, surname, firstname, email, password, avatar ,id_role) VALUES ('Nelson12','Nelson','MANDELA','nelson@gmail.com','${userHashedPassword}','https://img.freepik.com/vecteurs-libre/jeune-prince-tenue-royale_1308-176144.jpgt=st=1733740941~exp=1733744541~hmac=12f72461b777d275eef326d781be2624188f5a5b878ffd451426d92c62254e40&w=740',2)`
 		);
 
-		
 		// ---------Create Posts---------//
-		await db.execute(`INSERT INTO posts( title, image, content, id_user) VALUES ('Bienvenus à vous','https://img.freepik.com/photos-premium/modele-conception-post-medias-sociaux-du-restaurant_1029473-146673.jpg?w=740','Plongez dans une expérience culinaire unique où saveurs et convivialité se rencontrent. Que vous soyez amateur de plats traditionnels ou curieux de découvrir des créations originales, notre chef met tout son savoir-faire dans chaque assiette.',1)`);
-		
+		await db.execute(
+			`INSERT INTO posts( title, image, content, id_user) VALUES ('Bienvenus à vous','https://img.freepik.com/photos-premium/modele-conception-post-medias-sociaux-du-restaurant_1029473-146673.jpg?w=740','Plongez dans une expérience culinaire unique où saveurs et convivialité se rencontrent. Que vous soyez amateur de plats traditionnels ou curieux de découvrir des créations originales, notre chef met tout son savoir-faire dans chaque assiette.', 2)`
+		);
+
 		console.log("✅ Database seeded successfully ✅");
 		process.exit(0);
 	} catch (error) {
