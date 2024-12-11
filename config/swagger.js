cf1db8ec1ab3897b67049127f2e5d777d9721eca
Waris Aremou <warisaremou2003@gmail.com>
2024-12-11 17:16:35 +0100

    update swagger

--- config/swagger.js
+++ config/swagger.js
@@ -2,20 +2,11 @@ import swaggerJsdoc from "swagger-jsdoc";
 
 const swaggerOptions = {
 	definition: {
-		openapi: "3.1.0",
+		openapi: "3.0.0",
 		info: {
 			title: "BlogHunter API documentation",
 			version: "0.1.0",
 			description: "Cms Blog API documentation made with Express and documented with Swagger",
-			// license: {
-			// 	name: "MIT",
-			// 	url: "https://spdx.org/licenses/MIT.html",
-			// },
-			// contact: {
-			// 	name: "LogRocket",
-			// 	url: "https://logrocket.com",
-			// 	email: "info@email.com",
-			// },
 		},
 		servers: [
 			{
