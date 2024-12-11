c680daf30e3a0ee0934689a949422c7bf2baaa37
Waris Aremou <warisaremou2003@gmail.com>
2024-12-11 11:47:35 +0100

    update: add middleware on comment and categories routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { create, getAll, getOne, remove, update } from "../controllers/category_controller.js";
+import { authMiddleware, isAdminMiddleware } from "../middleware/auth_middleware.js";
 import { createCategoryValidation } from "../validators/category_validator.js";
 
 const router = express.Router();
@@ -13,10 +14,10 @@ router.get("/", getAll);
 // ----------- GET A CATEGORY BY ID ----------- //
 router.get("/:id", getOne);
 // ----------- ADD A CATEGORY ----------- //
-router.post("/", createCategoryValidation, create);
+router.post("/", authMiddleware, isAdminMiddleware, createCategoryValidation, create);
 // ----------- UPDATE A CATEGORY BY ID ----------- //
-router.patch("/:id", createCategoryValidation, update);
+router.patch("/:id", authMiddleware, isAdminMiddleware, createCategoryValidation, update);
 // ----------- DELETE A CATEGORY BY ID ----------- //
-router.delete("/:id", remove);
+router.delete("/:id", authMiddleware, isAdminMiddleware, remove);
 
 export default router;
